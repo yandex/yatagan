c994be334b38104f93ba41021f668a86347d94fd
Fedor Ihnatkevich <jeffset@yandex-team.ru>
2022-06-16 14:41:46 +0300

    DAGGERLITE-50: Make `BiObjectCache` thread-safe.
    This was overlooked, while only ObjectCache was made thread-safe.

--- base/src/main/kotlin/BiObjectCache.kt
+++ base/src/main/kotlin/BiObjectCache.kt
@@ -5,19 +5,23 @@ package com.yandex.daggerlite.base
  *
  * @See ObjectCache
  */
-@Suppress("LeakingThis")
 abstract class BiObjectCache<K1, K2, V : Any> : ObjectCacheBase() {
     protected val cache = hashMapOf<K1, MutableMap<K2, V>>()
 
     init {
+        @Suppress("LeakingThis")
         ObjectCacheRegistry.register(this)
     }
 
     protected inline fun createCached(key1: K1, key2: K2, block: () -> V): V {
-        return cache.getOrPut(key1, ::hashMapOf).getOrPut(key2) { block() }
+        synchronized(this) {
+            return cache.getOrPut(key1, ::hashMapOf).getOrPut(key2) { block() }
+        }
     }
 
     override fun clear() {
-        cache.clear()
+        synchronized(this) {
+            cache.clear()
+        }
     }
 }
