6e224eb19cf7a7a8770561c9e9c6d5947d3403e5
Fedor Ihnatkevich <jeffset@yandex-team.ru>
2023-03-31 15:58:35 +0300

    Chore: Nexus Publish Plugin 1.3.0

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -11,7 +11,7 @@ dependencyResolutionManagement {
 
             library("kotlin-gradle", "org.jetbrains.kotlin", "kotlin-gradle-plugin").versionRef("kotlin")
             library("dokka-gradle", "org.jetbrains.dokka", "dokka-gradle-plugin").versionRef("kotlin")
-            library("nexusPublish-gradle", "io.github.gradle-nexus:publish-plugin:1.1.0")
+            library("nexusPublish-gradle", "io.github.gradle-nexus:publish-plugin:1.3.0")
 
             library("ksp-api", "com.google.devtools.ksp", "symbol-processing-api").versionRef("ksp")
 
