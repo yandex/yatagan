a886c10ed842251bf66776f4c26f2b5e23181d62
Fedor Ihnatkevich <jeffset@yandex-team.ru>
2023-10-17 16:44:46 +0300

    Fix: Loops detection behind non-eager edges.
    There was the same bug in procedural tests generation, thus `Tall & Thin` contained a loop. This fixes the bug, and
    it can be seen in the golden code.

--- core/graph/impl/src/main/kotlin/loops.kt
+++ core/graph/impl/src/main/kotlin/loops.kt
@@ -16,7 +16,6 @@
 
 package com.yandex.yatagan.core.graph.impl
 
-import com.yandex.yatagan.base.ifOrElseNull
 import com.yandex.yatagan.base.traverseDepthFirstWithPath
 import com.yandex.yatagan.core.graph.BindingGraph
 import com.yandex.yatagan.core.graph.bindings.AliasBinding
@@ -24,6 +23,7 @@ import com.yandex.yatagan.core.graph.bindings.BaseBinding
 import com.yandex.yatagan.core.graph.bindings.Binding
 import com.yandex.yatagan.core.graph.component1
 import com.yandex.yatagan.core.graph.component2
+import com.yandex.yatagan.core.model.DependencyKind
 import com.yandex.yatagan.core.model.NodeDependency
 import com.yandex.yatagan.core.model.component1
 import com.yandex.yatagan.core.model.component2
@@ -31,33 +31,35 @@ import com.yandex.yatagan.core.model.isEager
 import com.yandex.yatagan.validation.Validator
 import com.yandex.yatagan.validation.format.Strings
 import com.yandex.yatagan.validation.format.reportError
-import kotlin.collections.component1
-import kotlin.collections.component2
 
 internal fun validateNoLoops(graph: BindingGraph, validator: Validator) {
-    traverseDepthFirstWithPath(
-        roots = buildList<BaseBinding> {
+    traverseDepthFirstWithPath<Pair<DependencyKind, BaseBinding>>(
+        roots = buildList {
             graph.entryPoints.forEach { (_, dependency) ->
-                add(graph.resolveBindingRaw(dependency.node))
+                add(dependency.kind to graph.resolveBindingRaw(dependency.node))
             }
             graph.memberInjectors.forEach {
                 it.membersToInject.forEach { (_, dependency) ->
-                    add(graph.resolveBindingRaw(dependency.node))
+                    add(dependency.kind to graph.resolveBindingRaw(dependency.node))
                 }
             }
         },
-        childrenOf = { binding ->
+        childrenOf = { (_, binding) ->
             class DependenciesVisitor : BaseBinding.Visitor<List<NodeDependency>> {
                 override fun visitAlias(alias: AliasBinding) = listOf(alias.source)
                 override fun visitBinding(binding: Binding) =
                     binding.dependencies + binding.nonStaticConditionProviders
             }
-            binding.accept(DependenciesVisitor()).mapNotNull { (node, kind) ->
-                ifOrElseNull(kind.isEager) { binding.owner.resolveBindingRaw(node) }
-            }.asIterable()
+            binding.accept(DependenciesVisitor()).map { (node, kind) ->
+                kind to binding.owner.resolveBindingRaw(node)
+            }
         },
         onLoop = { bindingLoop ->
-            validator.reportError(Strings.Errors.dependencyLoop(chain = bindingLoop.toList()))
+            // Check for lazy edges - if they are present, this is a "benign" loop, don't report it.
+            if (bindingLoop.all { (kind, _) -> kind.isEager }) {
+                val chain = bindingLoop.mapTo(arrayListOf()) { (_, binding) -> binding }
+                validator.reportError(Strings.Errors.dependencyLoop(chain = chain))
+            }
         }
     )
 }
